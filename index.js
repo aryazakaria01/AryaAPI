a64c907281580222c150b90160c2356f3fe28c34
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-02 08:41:02 +0700

    Prepare for release the version 6.2.8

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.GetLyrics = require('./lib/GetLyrics');
+exports.GetAlbumArt = require('./lib/GetAlbumArt');
+exports.GetSong = require('./lib/GetSong');
+exports.SearchSong = require('./lib/SearchSong');
+exports.GetSongById = require('./lib/GetSongById');
